ff66929982c738c9ff70e1bd02eba2f1af011a30
Khan Abdul Karim <axel127001@gmail.com>
2024-05-11 19:47:27 +0530

    updated

---
+++ script.js
@@ -0,0 +1,6 @@
+// var intervalID = setInterval(alert, 1000); // Will alert every second.
+// clearInterval(intervalID); // Will clear the timer.
+// function Alert(){
+//     alert(1)
+// }
+// setTimeout(alert, 1000); // Will alert once, after a second.
